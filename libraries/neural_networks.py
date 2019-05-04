a54881f2f6628b1f1b08fa4bec386a8811979ad5
Juan Rios <juandarr@gmail.com>
2019-05-03 23:59:29 -0500

    feat: complete first version of feed forwards process including hidden layers and multiple inputs.

--- libraries/neural_networks.py
+++ libraries/neural_networks.py
@@ -12,8 +12,9 @@ class NeuralNetworks:
 
     def __init__(self,input, output):
         self.size = [input,output]
-        self.hidden_size = 0
-        self.theta = [Matrix([[random() for j in range(self.size[0] + 1)] for i in range(self.size[-1])])]
+        self.hidden_size = []
+        self.total_layers = 2
+        self.init_weights()
 
     @staticmethod    
     def sigmoid(prod):
@@ -21,11 +22,42 @@ class NeuralNetworks:
             prod.matrix[0][i] = 1/(1+exp(-prod.matrix[0][i]))
         return prod
 
+    '''
+    Add hidden layer of dimension 'dimension'
+    '''
+    def add(self, dimension):
+        self.hidden_size.append(dimension)
+        self.total_layers += 1
+        self.init_weights()
+
+    def init_weights(self):
+        self.theta = []
+        layers_range = [self.size[0]]+ self.hidden_size + [self.size[-1]]
+        for i in range(len(layers_range)-1):
+            self.theta.append(Matrix([[random() for m in range(layers_range[i] + 1)] for n in range(layers_range[i+1])]))
+        #for i in range(len(self.theta)):
+        #    print(i, "\n\b", self.theta[i])
+
+    def getSize(self):
+        return [self.size[0]]+ self.hidden_size + [self.size[-1]]
+
     def feed_forward(self,x):
         for i in range(len(x)):
             x[i] = [1] + x[i]
-        prod = self.sigmoid(self.theta[0] * Matrix(x).transpose())
-        return prod
+        input_data = x
+        for i in range(len(self.theta)):
+            if i==0:
+                output_data = self.sigmoid(self.theta[i] * Matrix(x).transpose())
+            else:
+                output_data = self.sigmoid(self.theta[i] * input_data.transpose())
+                if (i==(len(self.theta)-1)):
+                    break
+            input_data = Matrix(output_data.matrix.copy()).transpose()
+
+            for j in range(len(input_data.matrix)):
+                input_data.matrix[j] = [1] + input_data.matrix[j]
+
+        return output_data
 
     def back_propagation(self):
         pass
@@ -38,10 +70,12 @@ if __name__=='__main__':
     #nn.theta.matrix[0] = [10,-15,-15]
     #print('\nOR gate')
     #nn.theta.matrix[0] = [-15,20,20]
-    print('\nAND gate')
-    y = [0,0,0,1]
-    nn.theta[0].matrix[0] = [-15,10,10]
+    #print('\nAND gate')
+    #y = [0,0,0,1]
+    #nn.theta[0].matrix[0] = [-15,10,10]
     x = [[0,0],[1,0],[0,1],[1,1]]
+    nn.add(5)
+    print('Network size: {}'.format(nn.getSize()))
     print([(i[0],i[1]) for i in x])
     y = nn.feed_forward(x)
     print(y)
