5d8414f73b6af6887f38871fa1dd21136ca4a966
Juan Rios <juandarr@gmail.com>
2019-05-03 21:53:40 -0500

    change: refined size and weights lists to include input, output, and multiple hidden layers.

--- libraries/neural_networks.py
+++ libraries/neural_networks.py
@@ -11,10 +11,9 @@ Features:
 class NeuralNetworks:
 
     def __init__(self,input, output):
-        self.input_size = input
-        self.output_size = output
+        self.size = [input,output]
         self.hidden_size = 0
-        self.theta = Matrix([[random() for j in range(self.input_size + 1)] for i in range(self.output_size)])
+        self.theta = [Matrix([[random() for j in range(self.size[0] + 1)] for i in range(self.size[-1])])]
 
     @staticmethod    
     def sigmoid(prod):
@@ -25,7 +24,7 @@ class NeuralNetworks:
     def feed_forward(self,x):
         for i in range(len(x)):
             x[i] = [1] + x[i]
-        prod = self.sigmoid(self.theta * Matrix(x).transpose())
+        prod = self.sigmoid(self.theta[0] * Matrix(x).transpose())
         return prod
 
     def back_propagation(self):
@@ -41,7 +40,7 @@ if __name__=='__main__':
     #nn.theta.matrix[0] = [-15,20,20]
     print('\nAND gate')
     y = [0,0,0,1]
-    #nn.theta.matrix[0] = [-15,10,10]
+    nn.theta[0].matrix[0] = [-15,10,10]
     x = [[0,0],[1,0],[0,1],[1,1]]
     print([(i[0],i[1]) for i in x])
     y = nn.feed_forward(x)
