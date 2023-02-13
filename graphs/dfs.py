87c831982e5bf134abb5a665982c92b9a1485ee1
Juan Rios <juanrios@tuta.io>
2023-02-13 13:53:30 -0500

    Added implementation of Breath First Search algorithm

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,1 +1,35 @@
-# I will continue later
+# Breath first search algorithm
+def bfs(graph, start):
+    # Queue used to explore layers of graph, one by one
+    to_explore = [start]
+    # Set of explored vertices
+    explored = set(start)
+    # Path to store visited nodes, in order
+    path = [start]
+    while to_explore:
+        currentVortex = to_explore.pop(0)
+        for vortex in graph[currentVortex]:
+            if vortex not in explored:
+                to_explore.append(vortex)
+                explored.add(vortex)
+                path.append(vortex)
+    return path
+
+
+def test_bfs(graph, start):
+    # Check if the result is what's expected
+    result = bfs(graph, start)
+    assert result == ['A', 'B', 'C', 'D', 'E', 'F'], f"Expected {result=} to\
+            be {['A', 'B', 'C', 'D', 'E', 'F']}"
+    print("Test passed")
+
+
+if __name__ == "__main__":
+    graph = {}
+    graph['A'] = ['B', 'C']
+    graph['B'] = ['A', 'D', 'E']
+    graph['C'] = ['A', 'F']
+    graph['D'] = ['B']
+    graph['E'] = ['B', 'F']
+    graph['F'] = ['C', 'E']
+    test_bfs(graph, 'A')
