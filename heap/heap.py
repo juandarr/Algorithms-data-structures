d2783348788ac5a4a37618dc9e9408c1ea1c9103
Juan Rios <juanrios@tuta.io>
2023-03-03 23:58:13 -0500

    Added function to show ancetry information for an arbitrary array

--- heap/heap.py
+++ heap/heap.py
@@ -11,3 +11,16 @@ def left(index: int) -> int:
 
 def right(index: int) -> int:
     return 2 * index + 2
+
+
+def show_ancestry(ar):
+    print("Node  Parent  Left  Right")
+    for idx, i in enumerate(ar):
+        n, p, l, r = (len(ar), parent(idx), left(idx), right(idx))
+        print(i, ar[p] if p < n and p >= 0 else None,
+              ar[l] if l < n else None,
+              ar[r] if r < n else None)
+
+
+ar = [10, 7, 2, 5, 1]
+show_ancestry(ar)
