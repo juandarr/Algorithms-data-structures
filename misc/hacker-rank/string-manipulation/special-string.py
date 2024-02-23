843f97d96d0e6f816e929fcc5cd24205dbc52631
Juan Ríos <juanrios@tuta.io>
2024-02-23 00:32:12 -0500

    refactor: added hash map to count repetition of characters and a character counter

--- misc/hacker-rank/string-manipulation/special-string.py
+++ misc/hacker-rank/string-manipulation/special-string.py
@@ -1,30 +1,37 @@
 def substrCount(n, s):
-    counter = 0
+    counter = n
     for i in range(n):
-        sub = s[i]
-        counter += 1
+        t = s[i]
+        k = 1
+        sub = {s[i]: 1}
         for j in range(i + 1, n):
-            sub += s[j]
-            print(sub)
-            tmp = set(sub)
-            if len(tmp) == 1:
+            t += s[j]
+            k += 1
+            if s[j] in sub:
+                sub[s[j]] += 1
+            else:
+                sub[s[j]] = 1
+            if len(sub) == 1:
                 counter += 1
-            elif len(tmp) == 2:
-                k = len(sub)
+            elif len(sub) == 2:
                 if k % 2 == 1:
-                    tmpChar = sub[k // 2]
-                    c = 0
-                    special = True
-                    for t in sub:
-                        if t == tmpChar:
-                            c += 1
-                            if c > 1:
-                                special = False
-                                break
-                    if special:
-                        counter += 1
+                    tmpChar = t[k // 2]
+                    if tmpChar in sub:
+                        if sub[tmpChar] == 1:
+                            counter += 1
+            else:
+                break
     return counter
 
 
-st = "mnonopoo"
-print(substrCount(len(st), st))
+file = open("input15.txt", "r")
+data = file.readlines()
+idx = 0
+for d in data:
+    if idx == 0:
+        n = int(d)
+        idx += 1
+    else:
+        st = d
+# st = "mnonopoo"
+print(substrCount(n, st))
