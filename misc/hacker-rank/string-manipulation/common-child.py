ea6deab0fb202a2b076c38d482650569cf6a6b7b
Juan Ríos <juanrios@tuta.io>
2024-02-29 11:02:23 -0500

    progress: initial function common-child works but not peformant for long strings

--- misc/hacker-rank/string-manipulation/common-child.py
+++ misc/hacker-rank/string-manipulation/common-child.py
@@ -1,7 +1,10 @@
-def commonChild(s1, s2):
+def commonChild(s1, s2, inc):
+    tmp = [inc]
     for idx1, i in enumerate(s1):
         for idx2, j in enumerate(s2):
             if i == j:
-                return 1 + commonChild(s1[idx1 + 1 :], s2[idx2 + 1 :])
-            continue
-    return 0
+                tmp.append(commonChild(s1[idx1 + 1 :], s2[idx2 + 1 :], inc + 1))
+    return max(tmp)
+
+
+print(commonChild("abcdef", "fbdamn", 0))
