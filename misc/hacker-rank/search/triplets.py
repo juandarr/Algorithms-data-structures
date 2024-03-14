1809500b5eeab25824097ab5278e601c84f28362
Juan Ríos <juanrios@tuta.io>
2024-03-14 16:49:34 -0500

    improve: better performance for solution of triplet sum challenge of search section, HackerRank

--- misc/hacker-rank/search/triplets.py
+++ misc/hacker-rank/search/triplets.py
@@ -1,15 +1,21 @@
 def triplets(a, b, c):
-    tr = {}
+    a = set(a)
+    b = set(b)
+    c = set(c)
+    a_s = sorted(a)
+    c_s = sorted(c)
+    count = 0
     for j in b:
-        for i in a:
+        for i in a_s:
             if j >= i:
-                for k in c:
+                for k in c_s:
                     if j >= k:
-                        if (i, j, k) not in tr:
-                            tr[(i, j, k)] = 1
+                        count += 1
+                    else:
+                        break
             else:
-                continue
-    return len(tr)
+                break
+    return count
 
 
 print(triplets([1, 3, 5], [2, 3], [1, 2, 3]))
