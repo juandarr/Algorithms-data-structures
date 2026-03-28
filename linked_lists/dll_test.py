a08a26655a319e9176aa3b94abf293487ae16cc0
Juan Rios <juanrios@tuta.io>
2026-03-27 23:59:55 -0500

    add: test file to test double linked list class

---
+++ linked_lists/dll_test.py
@@ -0,0 +1,33 @@
+from .dll import Node, Double_linked_list
+
+# List sample to convert from list to linked list
+ar = [1, 3, 5, 7, 0, 2, 10]
+
+# Stores elements of list in array of nodes, initializes linked list with first
+# element
+nodes = []
+start_node = Node(ar[0])
+nodes.append(start_node)
+ll = Double_linked_list(start_node)
+for value in ar[1:]:
+    new_node = Node(value)
+    new_node.prev = nodes[-1]
+    nodes[-1].next = new_node
+    nodes.append(new_node)
+ll.last = nodes[-1]
+
+
+# Traverse double linked list forward
+node = ll.first
+print('Traversing forward')
+while (node is not None):
+    print(node.data)
+    node = node.next
+
+
+# Traverse double linked list backward
+node = ll.last
+print('\nTraversing backward')
+while (node is not None):
+    print(node.data)
+    node = node.prev
